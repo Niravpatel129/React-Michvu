5e507598a7f8370dfba45410a32ca740e3770563
Niravpatel129 <niravpatelp129@gmail.com>
2020-01-10 23:27:28 -0500

    changes

--- src/components/TitleImage/TitleImage.js
+++ src/components/TitleImage/TitleImage.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./TitleImage.scss";
-import image from "./pig-wolf1.png";
 import ScrollAnimation from "react-animate-on-scroll";
+import ModalImage from "react-modal-image";
 
 function TitleImage({ title, src }) {
   return (
     <ScrollAnimation animateIn="fadeIn">
       <section className="TitleImage">
         {title && <h3>{title}</h3>}
-        <img alt={src || image} src={src || image}></img>
+        {/* <img alt={src || image} src={src || image}></img> */}
+        <ModalImage small={src} large={src} />
       </section>
     </ScrollAnimation>
   );
