b12c429e2dcf87aecbe33987c94ed2ba74ca8557
Niravpatel129 <niravpatelp129@gmail.com>
2020-01-21 17:23:45 -0500

    changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -19,7 +19,7 @@ function Navbar() {
         <h4>Michelle Vu</h4>
       </Link>
       <h2>TORONTO</h2>
-      <h2>LAST UPDATED: 07/26/19</h2>
+      <h2>LAST UPDATED: 01/10/2020</h2>
       <ScrollIntoView selector="#contact">
         <a
           href="/#contact"
