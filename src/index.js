5bc5b8e65fb9364e2f39c8b9d3369353488824dd
Nirav <nirav@alfangroup.com>
2020-06-25 17:55:37 -0400

    Maintenance

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "animate.css/animate.min.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'animate.css/animate.min.css';
 
-import "./index.css";
-ReactDOM.render(<App />, document.getElementById("root"));
+import './index.css';
+
+const Maintenance = () => {
+  return <h1>Page is under Maintenance, contact me at mivuuuu@gmail.com.</h1>;
+};
+
+ReactDOM.render(<Maintenance />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
